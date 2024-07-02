79e134676a6f94d9541a78999ac3c4365f36791c
Astridrdzza03 <21360000607@utna.edu.mx>
2024-07-02 14:27:49 -0600

    accept

--- routes/comida.route.js
+++ routes/comida.route.js
@@ -1,16 +1,6 @@
 import {Router} from "express";
 const route = Router();
 import comidaCtrl from "../controllers/comida.controller.js";
-<<<<<<< HEAD
-
-route.post('/', comidaCtrl.createComida);
-route.get('/', comidaCtrl.listAllComida);
-route.get('/:key/:value', comidaCtrl.searchComida);
-route.put('/:key/:value', comidaCtrl.updateComida);
-route.put('/many/:key/:value', comidaCtrl.updateComidaMany);
-route.delete('/:key/:value', comidaCtrl.deleteComida);
-route.delete('/many/:key/:value', comidaCtrl.deleteComidaMany);
-=======
 import { authMiddleware } from '../middlewares/Auth.js';
 
 route.post('/', authMiddleware, comidaCtrl.createComida);
@@ -20,6 +10,5 @@ route.put('/:key/:value', authMiddleware, comidaCtrl.updateComida);
 route.put('/many/:key/:value', authMiddleware, comidaCtrl.updateComidaMany);
 route.delete('/:key/:value', authMiddleware, comidaCtrl.deleteComida);
 route.delete('/many/:key/:value', authMiddleware, comidaCtrl.deleteComidaMany);
->>>>>>> origin/JWT
 
 export default route;
