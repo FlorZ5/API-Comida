f31a6d945fe807a02635b83b50ab74af896771db
Astridrdzza03 <21360000607@utna.edu.mx>
2024-07-02 13:17:59 -0600

    update database

--- database.js
+++ database.js
@@ -1,12 +1,15 @@
-import mongoose, { Mongoose } from "mongoose";
-const uri = "mongodb://127.0.0.1:27017/comida"; 
-//Creamos la conexión
-const connectDB=async()=>{
+import mongoose from 'mongoose';
+
+const uri = "mongodb+srv://2136000607:Galleta06@cluster0.9pd4gyf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"; // URI de conexión a MongoDB Atlas
+
+// Creamos la conexión
+const connectDB = async () => {
   try {
     const db = await mongoose.connect(uri);
-    console.log('Base de datos conectada: ',db.connection.name);
+    console.log('Base de datos conectada:', db.connection.name);
   } catch (error) {
-    console.log('Error:',error.message);
+    console.error('Error de conexión a la base de datos:', error.message);
+    process.exit(1); // Termina la ejecución del proceso en caso de error de conexión
   }
 }
 
